53665190e4f04dbec078f0f442d2bbd7bd5419d9
caarlosortegaa <carlosortegagimenez11@gmail.com>
2023-09-18 17:22:37 +0200

    18/09/2023

---
+++ AplicacionParaPruebas/AplicacionParaPruebas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AplicacionParaPruebas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pruebas.highterNumber(8,7,8));
+        }
+    }
+}
---
+++ AplicacionParaPruebas/AplicacionParaPruebas/Pruebas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionParaPruebas
+{
+    public class Pruebas
+    {
+        public bool IsSecondParameterHigher(double n1, double n2)
+        {
+            return n1 < n2;
+        }
+
+        public bool IsEven(int  n1)
+        {
+            return n1 % 2 == 0;
+        }
+
+        public static int highterNumber(int n1, int n2, int n3)
+        { 
+            if (n2 >= n1 && n2 >= n3)
+                return n2;
+            if(n3 >= n1 && n3 >= n2)
+                return n3;
+            return n1;
+
+            
+        }
+    }
+}
