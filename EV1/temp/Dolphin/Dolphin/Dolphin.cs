b24b7f648ddcc585aa2b6ecaa81a5decfd868106
caarlosortegaa <carlosortegagimenez11@gmail.com>
2023-09-25 18:21:24 +0200

    25/09/2023

---
+++ EV1/temp/Dolphin/Dolphin/Dolphin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolphin
+{
+    public enum ColorType
+    {
+        BLACK,
+        GREY,
+        BLUE
+    }
+    public class Dolphin
+    {
+        public double life;
+        public double size;
+        public string name;
+        public ColorType type;
+
+        public double GetLifePercent()
+        {
+            return life / 10;
+        }
+    }
+}
---
+++ EV1/temp/Dolphin/Dolphin/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Dolphin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolphin d1;
+            d1 = new Dolphin();
+
+            d1.name = "Juan";
+            d1.life = 270.0;
+            d1.size = 5.0;
+            d1.type = ColorType.BLACK;
+
+            double l;
+            l = d1.GetLifePercent();
+           
+        }
+    }
+}
