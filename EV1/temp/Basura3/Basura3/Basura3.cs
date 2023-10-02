8eea76d8c3643afbb5daecc4b0144f7db8fe9610
caarlosortegaa <carlosortegagimenez11@gmail.com>
2023-10-02 18:11:47 +0200

    02/10/23

---
+++ EV1/temp/Basura3/Basura3/Basura3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basura3
+{
+    public class Basura3
+    {
+        
+
+        public static int CountElements(List<string> list)
+        {
+            return list.Count;
+        }
+
+        public static void GreaterThanZroCount(List<double> List) // Funcion que te imprime los numeros que hay por encima de 0
+        {
+           
+            for(int i = 0; i < List.Count; i++)
+            {
+                if (List[i] > 0)
+                {
+                    Console.WriteLine(List[i]);
+                }
+            }
+        }
+        public static int GreaterThanZeroCount(List<double> List) //Funcion que te cuenta cuantos numeros hay por encima de 0
+        {
+            int result = 0;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (List[i] > 0)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ EV1/temp/Basura3/Basura3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Basura3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          List<double> list = new List<double>();
+            list.Add(1.0);
+            list.Add(-5.5);
+            list.Add(3.5);
+            Basura3.GreaterThanZroCount(list);
+            Console.WriteLine(Basura3.GreaterThanZeroCount(list));
+        }
+    }
+}
