9f323cd261be40184ee288f1e144314f75fb0b55
caarlosortegaa <carlosortegagimenez11@gmail.com>
2023-10-04 18:57:54 +0200

    04/10/23

--- EV1/temp/Basura3/Basura3/Basura3.cs
+++ EV1/temp/Basura3/Basura3/Basura3.cs
@@ -14,6 +14,10 @@ namespace Basura3
         {
             return list.Count;
         }
+        public static int CountElementsInt(List<int> list)
+        {
+            return list.Count;
+        }
 
         public static void GreaterThanZroCount(List<double> List) // Funcion que te imprime los numeros que hay por encima de 0
         {
@@ -39,6 +43,67 @@ namespace Basura3
             return result;
         }
 
+        public static bool IsInTheList(List<int> ListInt , int num)
+        {
+            if (ListInt == null)
+                return false;
+            for(int i = 0; i < ListInt.Count; i++)
+            {
+                if (ListInt[i] == num)
+                    return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Funcion que devuelve el valor mayor de una lista de enteros
+        /// </summary>
+        /// <param name="IntList">Lista de enteros</param>
+        /// <returns>Numero mas alto, si la lista es null devuelve int.MinValue</returns>
+        public static int higherNumberInTheList(List<int> IntList)
+        {
+            if (IntList == null)
+                return int.MinValue;
+
+            int higherNumber = int.MinValue;
+            for(int i = 0; i < IntList.Count; i++)
+            {
+                if (IntList[i] > higherNumber)
+                    higherNumber = IntList[i];
+            }
+            return higherNumber;
+        }
+
+        public static int HighterNumberPositionInTheList(List<int> ListInt)
+        {
+            if (ListInt == null || ListInt.Count == 0)
+                return -1;
+
+            int Max= 0;
+            int index = 0;
+            for(var i = 0; i < ListInt.Count; i++)
+            {
+                if (ListInt[i] > Max)
+                {
+                    Max = ListInt[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+        public static bool IsInOrder(List<int> ListInt)
+        {
+            if (ListInt == null || ListInt.Count == 0)
+                return false;
+            int number = ListInt[0];
+            for(int i = 1; i < ListInt.Count; i++)
+            {
+                if (ListInt[i] < number)
+                    return false;
+                number = ListInt[i];
+
+            }
+            return true;
+        }
 
     }
 }
--- EV1/temp/Basura3/Basura3/Program.cs
+++ EV1/temp/Basura3/Basura3/Program.cs
@@ -4,12 +4,35 @@
     {
         static void Main(string[] args)
         {
-          List<double> list = new List<double>();
-            list.Add(1.0);
-            list.Add(-5.5);
-            list.Add(3.5);
-            Basura3.GreaterThanZroCount(list);
-            Console.WriteLine(Basura3.GreaterThanZeroCount(list));
+          List<double> ListDouble = new List<double>();
+            ListDouble.Add(1.0);
+            ListDouble.Add(-5.5);
+            ListDouble.Add(3.5);
+            //Basura3.GreaterThanZroCount(ListDouble);
+            //Console.WriteLine(Basura3.GreaterThanZeroCount(ListDouble));
+
+
+            List<int> IntList = new List<int>();
+            //for(int i = 0; i < 67; i++)
+            //    IntList.Add(i);
+            //Console.WriteLine(Basura3.IsInTheList(IntList, 67));
+
+            IntList.Add(-10);
+            IntList.Add(-9);
+            IntList.Add(-8);
+            IntList.Add(0);
+            IntList.Add(1);
+            IntList.Add(3);
+            IntList.Add(4);
+            IntList.Add(5);
+            IntList.Add(4);
+
+            //Console.WriteLine(Basura3.higherNumberInTheList(IntList));
+            //Console.WriteLine(Basura3.HighterNumberPositionInTheList(IntList));
+            Console.WriteLine(Basura3.IsInOrder(IntList));
+            Console.WriteLine(Basura3.CountElementsInt(IntList));
+
+
         }
     }
 }
