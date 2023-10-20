11a8ba2dc67f1d252cf725eb2e4033d0d524054e
caarlosortegaa <carlosortegagimenez11@gmail.com>
2023-10-20 17:43:37 +0200

    20/10/23

--- EV1/temp/BasuraFrame/BasuraFrame/MiJuego.cs
+++ EV1/temp/BasuraFrame/BasuraFrame/MiJuego.cs
@@ -9,66 +9,46 @@ namespace BasuraFrame
 {
     public class MiJuego : UDK.IGameDelegate
     {
-        List<Character> jugadores = new List<Character>();
-        Character pj1 = new Character();
-        Character pj2 = new Character();
-        Character pj3 = new Character();
-
+        Mundo? Mundo;
+       
         public void OnLoad(GameDelegateEvent gameEvent)
         {
-            CreateCharacter();
+            Mundo = new Mundo();
+            Mundo.CreateCharacter();
         }
         public void OnKeyboard(GameDelegateEvent gameEvent, IKeyboard keyboard, IMouse mouse)
         {
-            for (int i = 0; i < jugadores.Count; i++)
-            {
+            
                 if (keyboard.IsKeyDown(Keys.Right))
-                    jugadores[i].x += 3.5f;
+                    Mundo.GetCharacterAt(0).x += 3.5f;
                 if (keyboard.IsKeyDown(Keys.Left))
-                    jugadores[i].x -= 3.5f;
+                    Mundo.GetCharacterAt(0).x -= 3.5f;
                 if (keyboard.IsKeyDown(Keys.Up))
-                    jugadores[i].y += 3.5f;
+                    Mundo.GetCharacterAt(0).y += 3.5f;
                 if (keyboard.IsKeyDown(Keys.Down))
-                    jugadores[i].y -= 3.5f;
-            }
+                    Mundo.GetCharacterAt(0).y -= 3.5f;
+            
 
         }
         public void OnAnimate(GameDelegateEvent gameEvent)
         {
-            for (int i = 0; i < jugadores.Count; i++)
+            for (int i = 0; i < Mundo.GetCharacterCount(); i++)
             {
-                jugadores[i].x += utils.GetRandomReal(-1, 1);
-                jugadores[i].y += utils.GetRandomReal(-2, 2);
+                Mundo.GetCharacterAt(i).x += utils.GetRandomReal(-1, 1);
+                Mundo.GetCharacterAt(i).y += utils.GetRandomReal(-2, 2);
             }
         }
 
         public void OnDraw(GameDelegateEvent gameEvent, ICanvas canvas)
         {
 
-            canvas.Clear(0.0, 0.0, 0.0, 1.0);
-            canvas.Camera.SetRectangle(0, 0, canvas.Width, canvas.Height);
-            for (int i = 0; i < jugadores.Count; i++)
-            {
-                Character ch = jugadores[i];
-                ch.Draw(canvas);
-            }
+            Mundo.DrawWorld(canvas);
         }
 
         public void OnUnload(GameDelegateEvent gameEvent)
         {
 
         }
-        public void CreateCharacter()
-        {
-            for (int i = 0; i < 1000; i++)
-            {
-                Character j = new Character();
-                j.r = utils.GetRandom();
-                j.g = utils.GetRandom();
-                j.b = utils.GetRandom();
-                j.a = utils.GetRandom();
-                jugadores.Add(j);
-            }
-        }
+       
     }
 }
--- EV1/temp/BasuraFrame/BasuraFrame/Mundo.cs
+++ EV1/temp/BasuraFrame/BasuraFrame/Mundo.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UDK;
 
 namespace BasuraFrame
 {
     public class Mundo
     {
+       private List<Character> _jugadores = new List<Character>();
+        public int GetCharacterCount()
+        { 
+            return _jugadores.Count; 
+        }
+        public Character? GetCharacterAt(int index)
+        {
+            if(index < 0 || index >= _jugadores.Count)
+                return null;
+            return _jugadores[index];
+        }
+        public void CreateCharacter()
+        {
+            
+            for (int i = 0; i < 200; i++)
+            {
+                Character j = new Character();
+                j.r = utils.GetRandom();
+                j.g = utils.GetRandom();
+                j.b = utils.GetRandom();
+                j.a = utils.GetRandom();
+                _jugadores.Add(j);
+            }
+        }
 
+        public void DrawWorld(ICanvas canvas)
+        {
+            canvas.Clear(0.0, 0.0, 0.0, 1.0);
+            canvas.Camera.SetRectangle(0, 0, canvas.Width, canvas.Height);
+            for (int i = 0; i < _jugadores.Count; i++)
+            {
+                Character ch = _jugadores[i];
+                ch.Draw(canvas);
+            }
+        }
     }
 }
