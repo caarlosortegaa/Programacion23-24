12db158569c4d0befa241d2a8c91ca0154d51a7f
caarlosortegaa <carlosortegagimenez11@gmail.com>
2023-11-03 18:50:21 +0100

    03/11/23

--- PROG/EV1/Classes/Classes/Chessfigure.cs
+++ PROG/EV1/Classes/Classes/Chessfigure.cs
@@ -3,9 +3,10 @@
     
     public class Chessfigure
     {
-        int _x, _y;
+        int _x, _y, movementCount;
         ColorType _color;
         FigureType _type;
+       
         public Chessfigure(int x, int y, FigureType type , ColorType color)
         {
             _x = x;
@@ -24,7 +25,7 @@
         }
         public bool IsValid()
         {
-            if(_x < 0 || _y < 0)
+            if(_x < 0 || _y < 0 || _x > 8 || _y > 8)
                 return false;
             return true;
         }
@@ -36,11 +37,22 @@
         {
             return _y;
         }
+        public FigureType Getfigure()
+        {
+            return _type;
+        }
+        public int GetMovementCount()
+        {
+            return movementCount;
+        }
+        public bool HasBeenMoved()
+        {
+            return movementCount > 0;
+        }
 
-        
         public void moveTo(int x, int y)
         {
-
+            
         }
         
 
---
+++ PROG/EV1/Classes/Classes/UtilsFigure.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Classes
+{
+    public class UtilsFigure
+    {
+        public static Chessfigure? GetFigureAt(List<Chessfigure> ListFigure , int x , int y, Chessfigure figure)
+        {
+            if(ListFigure == null || figure.IsValid() == false)
+                return null;
+            for(int i = 0; i < ListFigure.Count; i++)
+            {
+                if (ListFigure[i].GetX() == x && ListFigure[i].GetY() == y)
+                    return ListFigure[i];
+            }
+            return null;
+        }
+
+    }
+}
