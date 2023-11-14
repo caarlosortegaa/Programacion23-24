9c3cccf6b69b1d72f3d0db7f331c9548f5cd3266
caarlosortegaa <118833961+caarlosortegaa@users.noreply.github.com>
2023-11-14 14:17:37 +0100

    casa

--- PROG/EV1/Classes/Classes/DateTime.cs
+++ PROG/EV1/Classes/Classes/DateTime.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Classes
 {
@@ -13,6 +9,7 @@ namespace Classes
     public class DateTime
     {
         private int _second, _minute, _hour, _day, _month, _year;
+        DayOfWeek _dayOfWeek;
 
         public DateTime(int second, int minute, int hour, int day, int month, int year)
         {
@@ -23,35 +20,70 @@ namespace Classes
             _month = month;
             _year = year;
         }
-        public int Second()
+        public DateTime(int day , int month , int year)
+        {
+            _day = day;
+            _month = month;
+            _year = year;
+        }
+        public int GetSecond()
         {
             return _second;
         }
-        public int Minute()
+        public int GetMinute()
         {
             return _minute;
         }
-        public int Hour()
+        public int GetHour()
         {
             return _hour;
         }
-        public int Day()
+        public int GetDay()
         {
             return _day;
         }
-        public int Month()
+        public int GetMonth()
         {
             return _month;
         }
-        public int Year()
+        public int GetYear()
         {
             return _year;
         }
+        public DayOfWeek GetDayOfWeek()
+        {
+            return _dayOfWeek;
+        }
+        //public DateTime Clone()
+        //{
+        //}
         public bool IsValid()
         {
             if(_second < 0 || _minute < 0 || _hour < 0 || _day < 0 || _month < 0 ||_year < 0)
                 return false;
             return true;
+        }
+        public static bool IsLeap(int year)
+        {
+            return year % 4 == 0 && year % 100 != 0 ? true : false;
+        }
+        public string DateToString()
+        {
+            return  _day + "/" + _month + "/" + _year;
+        }
+        public int GetDaysCount(int year, int month)
+        {
+            int count = 0;
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                count += 31;
+            if (month == 4 || month == 6 || month == 9 || month == 11)
+                count += 30;
+            if (!IsLeap(year) && month == 2)
+                count += 28;
+            if (IsLeap(year) && month == 2)
+                count += 29;
+            return count;
+            
         }
 
 
--- PROG/EV1/Classes/Classes/Program.cs
+++ PROG/EV1/Classes/Classes/Program.cs
@@ -4,14 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Card c = new Card(Palo.CORAZONES, 1);
-            Card c1 = new Card(Palo.CORAZONES, 11);
-            Card c2 = new Card(Palo.TREBOLES, 0);
-            List<Card> card = new List<Card>();
-            card.Add(c);
-            card.Add(c1);
-            card.Add(c2);
-            Console.WriteLine(UtilsCard.BlackJack(card));
+            //Card c = new Card(Palo.CORAZONES, 1);
+            //Card c1 = new Card(Palo.CORAZONES, 11);
+            //Card c2 = new Card(Palo.TREBOLES, 0);
+            //List<Card> card = new List<Card>();
+            //card.Add(c);
+            //card.Add(c1);
+            //card.Add(c2);
+            //Console.WriteLine(UtilsCard.BlackJack(card));
+            DateTime d = new DateTime(15, 12, 2003);
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(d.GetDaysCount(2013 , 2));
 
             
         }
