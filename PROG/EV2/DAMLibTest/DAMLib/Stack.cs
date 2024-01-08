9d13a26a25a86920e3cebbf14551fd1b521aa000
caarlosortegaa <carlosortegagimenez11@gmail.com>
2024-01-08 16:40:02 +0100

    08/01/24

---
+++ PROG/EV2/DAMLibTest/DAMLib/Stack.cs
@@ -0,0 +1,25 @@
+﻿namespace DAMLib
+{
+    public class Stack <T>
+    {
+        private T[] _stack;
+
+        public void Push(T newElement)
+        {
+
+        }
+        public T Pop()
+        {
+            return _stack[_stack.Length - 1];
+        }
+        public T GetTop()
+        {
+            return _stack[0];
+        }
+        public bool IsEmpty()
+        {
+
+        }
+        public int GetCount() => _stack.Length;
+    }
+}
---
+++ PROG/EV2/DAMLibTest/DAMLibTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using DAMLib;
+
+namespace DAMLibTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s;
+        }
+    }
+}
