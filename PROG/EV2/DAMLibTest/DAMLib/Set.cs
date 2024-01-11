e3a7f1102d9b7e234ca9e111d8c5340568e2a68a
caarlosortegaa <carlosortegagimenez11@gmail.com>
2024-01-11 16:59:44 +0100

    set

--- PROG/EV2/DAMLibTest/DAMLib/Set.cs
+++ PROG/EV2/DAMLibTest/DAMLib/Set.cs
@@ -8,21 +8,47 @@
         public void Add(T element)
         {
             T[] array = new T[_set.Length + 1];
-            for (int i = 0; i < array.Length; i++)
+            if (!Contains(element))
             {
-                array[i] = _set[i];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = _set[i];
+                }
             }
             array[array.Length - 1] = element;
             _set = array;
         }
         public void Remove(T element)
         {
-            T[] array = new T[_set.Length - 1];
-            for (int i = 0; i < _set.Length - 1; i++)
+            int indexValue = IndexOf(element);
+            if (indexValue != -1)
             {
-                array[i] = _set[i];
+                T[] array = new T[_set.Length - 1];
+                for (int i = 0; i < _set.Length; i++)
+                {
+                    if(indexValue != i)
+                    {
+                        array[i] = _set[i];
+                    }
+                }
+
+                _set = array;
             }
-           _set = array;
+        }
+        public bool Contains(T element)
+        {
+            if(IndexOf(element) != -1) 
+                return true;
+            return false;
+        }
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < _set.Length; i++)
+            {
+                if (_set[i].Equals(element))
+                    return i;
+            }
+            return -1;
         }
     }
 }
