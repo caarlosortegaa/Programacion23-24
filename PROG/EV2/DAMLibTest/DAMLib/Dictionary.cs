7ea1ff65e056418ff3c33c3dcb245d8acaedb626
caarlosortegaa <carlosortegagimenez11@gmail.com>
2024-01-22 17:43:07 +0100

    220124

---
+++ PROG/EV2/DAMLibTest/DAMLib/Dictionary.cs
@@ -0,0 +1,31 @@
+﻿namespace DAMLib
+{
+    public class Dictionary<K, V>
+    {
+        private class item
+        {
+            public K key;
+            public V value;
+
+            public item(K key, V value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+        }
+        item[] _items = new item[0];
+        public void Add(K key, V value)
+        {
+
+        }
+        public void Remove(K key)
+        {
+
+        }
+        public Dictionary<K, V> Filter()
+        {
+
+        }
+
+    }
+}
