c557fa76606e5972a53304bb863b09565c0df3f3
caarlosortegaa <118833961+caarlosortegaa@users.noreply.github.com>
2024-01-30 15:24:13 +0100

    ewuifb

--- PROG/EV2/DAMLibTest/DAMLib/Node.cs
+++ PROG/EV2/DAMLibTest/DAMLib/Node.cs
@@ -1,53 +1,52 @@
-﻿namespace Tree
+﻿namespace DAMLib
 {
     public class Node<T>
     {
         public T Content { get; set; }
-        private List<Node<T>> _children = new List<Node<T>>();
-        private Node<T>? _parent { get; set;}
+        private List<Node<T>> _children;
+        private Node<T>? _parent { get; set; }
         public bool IsRoot => GetLevel() == 0;
-        public bool IsLeaf => _parent != null;
+        public bool IsLeaf => _children.Count == 0;
         public int ChildCount => _children.Count;
         public int Level => GetLevel();
         public Node<T> Root => _parent;
-        
+        public Node(T value)
+        {
+            Content = value;
+            _children = new List<Node<T>>();
+        }
+
 
         public int GetLevel()
         {
-            if(_parent == null)
+            if (_parent == null)
                 return 0;
             return _parent.GetLevel() + 1;
         }
-        //public Node<T> GetRooT()
-        //{
-            
-        //}
+        public Node<T> GetRooT()
+        {
+
+        }
         public Node<T>? GetChildrenAt(int index)
         {
             if (index < 0 || index >= _children.Count)
                 return null;
-            for(int i = 0; i < _children.Count; i++)
+            for (int i = 0; i < _children.Count; i++)
             {
                 if (index == i)
-                    return _children[i];                    
+                    return _children[i];
             }
             return null;
         }
         public void UnLink()
         {
-
+            if(_parent != null)
+                _parent._children.Remove(this);
         }
-        public void Detach()
-        {
-
-        }
-        public void ParentRemove()
-        {
-            
-        }
+        
         public void AddChild(Node<T> child)
         {
-            if(child == null)
+            if (child == null)
                 return;
             _children.Add(child);
         }
