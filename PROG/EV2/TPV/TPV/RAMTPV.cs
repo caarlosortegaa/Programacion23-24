51fc0416b3a1ac7998a808c875ef5bcdffd39f08
caarlosortegaa <carlosortegagimenez11@gmail.com>
2024-02-12 16:46:57 +0100

    tpv

---
+++ PROG/EV2/TPV/TPV/RAMTPV.cs
@@ -0,0 +1,33 @@
+﻿namespace TPV
+{
+    public class RAMTPV : ITPV
+    {
+        private Dictionary<long, Product> _products = new Dictionary<long, Product>();
+        public int ProductCount => _products.Count;
+        private long _CurrentGeneratingId = 1;
+
+        public long AddProduct(Product product)
+        {
+            var cloneProduct = product.Clone();
+            cloneProduct.id = _CurrentGeneratingId++;
+            _products.Add(product.id, cloneProduct);
+            return cloneProduct.id;
+            
+        }
+
+        public Product? GetProductWithId(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProductWithID(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProductWithId(long id, Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
