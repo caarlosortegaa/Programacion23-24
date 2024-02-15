e9ac15cf543a6faf3aaed0234c2b4825bc715e7d
caarlosortegaa <118833961+caarlosortegaa@users.noreply.github.com>
2024-02-15 14:17:00 +0100

    juan

--- PROG/EV2/TPVLib/TPV/Controllers.cs
+++ PROG/EV2/TPVLib/TPV/Controllers.cs
@@ -1,11 +1,12 @@
-﻿using TPVLib;
+﻿using System;
+using TPVLib;
 namespace TPV
 {
     public class Controllers
     {
         public void RunMainMenu(ITPV tpv)
         {
-
+            
         }
         public void RunProdcutsMenu(ITPV tpv)
         {
--- PROG/EV2/TPVLib/TPV/Program.cs
+++ PROG/EV2/TPVLib/TPV/Program.cs
@@ -1,11 +1,11 @@
 ﻿using TPVLib;
 namespace TPV
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+           
         }
     }
 }
