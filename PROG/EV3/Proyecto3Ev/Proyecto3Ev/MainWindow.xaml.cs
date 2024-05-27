a7a2ef3fcbe299396bbea9a61704f01cd45d6ce9
caarlosortegaa <118833961+caarlosortegaa@users.noreply.github.com>
2024-05-27 17:43:32 +0200

    5eytj

--- PROG/EV3/Proyecto3Ev/Proyecto3Ev/MainWindow.xaml.cs
+++ PROG/EV3/Proyecto3Ev/Proyecto3Ev/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto3Ev.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,36 @@ namespace Proyecto3Ev
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TBShow(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 0.5;
+        }
+
+        private void TBHide(object sender, RoutedEventArgs e)
+        {
+            GridContent.Opacity = 1;
+        }
+
+        private void PreviewLeftMouseButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            BtnShowHide.IsChecked = false;
+        }
+
+        private void Minimizar(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Cerrar(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Usuarios_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new Usuarios();
         }
     }
 }
